8c2c77eda4d355f02d91f3ef7b2ed0692072a89b
Selma Delgado <selmadelgado@gmail.com>
2018-07-29 20:01:05 -0500

    Create Random Result in Console

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,29 @@
-// Pseudo Code
-
-// A game with 4 Crystals and Random Value/Result 
-
-// The player is shown a random number and four crystals displayed as buttons on the page.
-
-// The random number at the start of the game is between 19-120
-
-// Each crystal has a random value between 1-12.
-
-// A new random number is generated and assigned to each crystal everytime we win or lose
-
-// When the player clicks on a crystal, the player's score counter is updated with that value until the total score is matches or exceeds the random number.
-
-// If the total score equals the random number, increment the win counter.
-
-// The player loses if their score exceeds the random number.
-
-// The game restarts whenever the player wins or loses.
-
-//The app keeps track of and shows the number of games the player wins and loses.
-
-
+//  Initialize variables
 var random_result;
-var loss;
-var win;
+var losses;
+var wins;
 
-for (var i = 0; i < 4 ; i++) {
-    console.log("Hello world!");
+
+// Create Random Result
+random_result = Math.floor(Math.random() * 120 )  + 19;  // generate random num between 19-120 - hoisting
+console.log(random_result);
+$("#result").html('Random Result:    '); // display Random Result in the DOM
+
+
+// Create HTML elements
+for (var i = 0;  i < 4;  i++ ) {
+         
+         var random = Math.floor(Math.random() * 12); // generate random num between 1-12
+         console.log(random);
+         var crystal = $("<div>"); // loop through 4 times to create 4 divs.
+                  
+                  // pipe random numbers into divs
+                  crystal.attr({
+                           "class": 'crystal',
+                           "data-random": random
+                  });
+         
+         $(".crystals").append(crystal); // pipe divs into container
+         console.log( "Hello, Leo!");
+         
 }
